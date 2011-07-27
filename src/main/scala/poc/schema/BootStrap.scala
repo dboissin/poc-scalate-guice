76f5ff2f3acea3f405522c2e917ae165c439e797
dboissin <damien.boissin@gmail.com>
2011-07-27 17:57:36 +0200

    Some tests.

--- src/main/scala/poc/schema/BootStrap.scala
+++ src/main/scala/poc/schema/BootStrap.scala
@@ -6,6 +6,7 @@ import org.squeryl.PrimitiveTypeMode._
 import poc.model.Cashier
 import poc.model.Timetable
 import java.sql.Timestamp
+import poc.model.WorkingTimeRange
 
 object BootStrap {
 
@@ -26,10 +27,36 @@ object BootStrap {
       create
       printDdl
 
-      cashiers.insert(new Cashier("Blip"))
-      cashiers.insert(new Cashier("Blop"))
+      val t = new Timetable(new Timestamp(System.currentTimeMillis), 1L)
+      val t2 = new Timetable(new Timestamp(System.currentTimeMillis+500000), 1L)
+      val blip = new Cashier("Blip")
+      cashiers.insert(blip)
+      println("blipId : " + blip.id)
+      blip.timetables.associate(t)
+      blip.timetables.associate(t2)
 
-      timetables.insert(new Timetable(new Timestamp(System.currentTimeMillis), 1L))
+      val c = cashiers.lookup(1L)
+      val titi = c.get.timetables.foldLeft(">>> ")((tmp, res) => tmp + res.id + ", " )
+      println (titi)
+
+      val blop = new Cashier("Blop")
+      println ("before insert => blopId : " + blop.id)
+      cashiers.insert(blop)
+      println ("after insert => blopId : " + blop.id)
+
+
+      val wtr = new WorkingTimeRange(new Timestamp(System.currentTimeMillis), new Timestamp(System.currentTimeMillis + 60000))
+      workingTimeRanges.insert(wtr)
+
+      //timetables.insert(t)
+      val t3 = timetables.where(t => t.id === 2L).single
+
+      t3.workingTimeRanges.associate(wtr)
+      t3.workingTimeRanges.dissociateAll
+      t3.workingTimeRanges.associate(wtr)
+//      schedules.associate(t, wtr)
+
+      //timetables.insert(new Timetable(new Timestamp(System.currentTimeMillis), 1L))
       val queried = cashiers.where(cashier => cashier.id === 2L).single
       println(queried.id + " -- " + queried.name)
 
